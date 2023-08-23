962e2f7f2ce76703c8195d5832773f611b43ad77
haol <lh15801619598@gmail.com>
2023-08-23 17:10:17 +0800

    refactor: No.1

---
+++ memory3D.cu
@@ -0,0 +1,26 @@
+#include <cuda_runtime.h>
+#include <iostream>
+
+__global__ void MyKernel(cudaPitchedPtr devPitchedPtr, int width, int height, int depth) {
+    char *devPtr = (char *)devPitchedPtr.ptr;
+    size_t pitch = devPitchedPtr.pitch;
+    size_t slicePitch = pitch * height;
+    for (int z = 0; z < depth; ++z) {
+        char *slice = devPtr + z * slicePitch;
+        for (int y = 0; y < height; ++y) {
+            float *row = (float *)(slice + y * pitch);
+            for (int x = 0; x < width; ++x) {
+                float element = row[x];
+            }
+        }
+    }
+}
+
+int main() {
+    int width = 64, height = 64, depth = 64;
+    cudaExtent extent = make_cudaExtent(width * sizeof(float), height, depth);
+    cudaPitchedPtr devPitchedPtr;
+    cudaMalloc3D(&devPitchedPtr, extent);
+    MyKernel<<<100, 512>>>(devPitchedPtr, width, height, depth);
+    return 0;
+}
---
+++ vectorAdd.cu
@@ -0,0 +1,90 @@
+#include <stdio.h>
+#include <cuda_runtime.h>
+
+__global__ void vectorAdd(const float *A, const float *B, float *C, int numElements) {
+    int i = blockDim.x * blockIdx.x + threadIdx.x;
+    if (i < numElements) {
+        C[i] = A[i] + B[i] + 0.0f;
+    }
+}
+
+int main() {
+    cudaError_t err = cudaSuccess;
+    int numElements = 50000;
+    size_t size = numElements * sizeof(float);
+    printf("[Vector addition of %d elementes]\n", numElements);
+
+    float *h_A = (float *)malloc(size);
+    float *h_B = (float *)malloc(size);
+    float *h_C = (float *)malloc(size);
+
+    if (h_A == NULL || h_B == NULL || h_C == NULL) {
+        fprintf(stderr, "Failed to allocate host vectors!\n");
+        exit(EXIT_FAILURE);
+    }
+
+    for (int i  =0; i < numElements; ++i) {
+        h_A[i] = rand() / (float) RAND_MAX;
+        h_B[i] = rand() / (float) RAND_MAX;
+    }
+
+    float *d_A = NULL;
+    err = cudaMalloc((void **)&d_A, size);
+    float *d_B = NULL;
+    err = cudaMalloc((void **)&d_B, size);
+    float *d_C = NULL;
+    err = cudaMalloc((void **)&d_C, size);
+
+    cudaMemcpy(d_A, h_A, size, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_B, h_B, size, cudaMemcpyHostToDevice);
+
+    int threadsPerBlock = 256;
+    int blocksPerGrid = (numElements + threadsPerBlock - 1) / threadsPerBlock;
+    printf("CUDA kernel launch with %d blocks of %d threads\n", blocksPerGrid,
+    threadsPerBlock);
+    vectorAdd <<< blocksPerGrid, threadsPerBlock >>>(d_A, d_B, d_C, numElements);
+    err = cudaMemcpy(h_C, d_C, size, cudaMemcpyDeviceToHost);
+
+    for (int i = 0; i < numElements; ++i) {
+        if (fabs(h_A[i] + h_B[i] - h_C[i]) > 1e-5) {
+          fprintf(stderr, "Result verification failed at element %d!\n", i);
+          exit(EXIT_FAILURE);
+        }
+      }
+    
+
+      printf("Test PASSED\n");
+
+      // Free device global memory
+      err = cudaFree(d_A);
+    
+      if (err != cudaSuccess) {
+        fprintf(stderr, "Failed to free device vector A (error code %s)!\n",
+                cudaGetErrorString(err));
+        exit(EXIT_FAILURE);
+      }
+    
+      err = cudaFree(d_B);
+    
+      if (err != cudaSuccess) {
+        fprintf(stderr, "Failed to free device vector B (error code %s)!\n",
+                cudaGetErrorString(err));
+        exit(EXIT_FAILURE);
+      }
+    
+      err = cudaFree(d_C);
+    
+      if (err != cudaSuccess) {
+        fprintf(stderr, "Failed to free device vector C (error code %s)!\n",
+                cudaGetErrorString(err));
+        exit(EXIT_FAILURE);
+      }
+    
+      // Free host memory
+      free(h_A);
+      free(h_B);
+      free(h_C);
+    
+      printf("Done\n");
+      return 0;
+}
